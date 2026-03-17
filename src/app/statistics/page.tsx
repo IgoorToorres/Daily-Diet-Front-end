543692b6780a255a8b6fb61bbac9a66e57ff0a92
IgoorToorres <igorevangelista121@gmail.com>
2026-03-17 19:14:36 -0300

    fix: ajuste visual stats page

--- src/app/statistics/page.tsx
+++ src/app/statistics/page.tsx
@@ -6,23 +6,29 @@ export default function Statistics() {
     <>
       <StatisticsHeader percentage={percentage} />
 
-      <div className="flex flex-col gap-5 items-center mt-10">
+      <div className="flex flex-col gap-5 items-center mt-5 p-5">
         <p className="font-bold text-gray-1">Estatísticas gerais</p>
-        <span className="text-2xl font-bold text-gray-1">{"22"}</span>
-        <p className="text-gray-2">
-          melhor sequência de pratos dentro da dieta
-        </p>
-        <span className="text-2xl font-bold text-gray-1">109</span>
-        <p className="text-gray-2">refeições registradas</p>
 
-        <div className="flex gap-3">
-          <div className="flex flex-col items-center justify-center gap-2 bg-green-light py-5 px-2 rounded-lg">
+        <div className=" w-full rounded-lg bg-gray-6 p-5 flex flex-col justify-center items-center">
+          <span className="text-2xl font-bold text-gray-1">{"22"}</span>
+          <p className="text-gray-2">
+            melhor sequência de pratos dentro da dieta
+          </p>
+        </div>
+
+        <div className=" w-full rounded-lg bg-gray-6 p-5 flex flex-col justify-center items-center">
+          <span className="text-2xl font-bold text-gray-1">109</span>
+          <p className="text-gray-2">refeições registradas</p>
+        </div>
+
+        <div className="flex justify-between gap-3 w-full">
+          <div className="flex flex-col items-center justify-center gap-2 bg-green-light px-2 py-5 rounded-lg">
             <span className="text-gray-2 text-3xl font-bold">99</span>
-            <p className="text-gray-3 ">refeições dentro da dieta</p>
+            <p className="text-gray-3 text-[14px]">refeições dentro da dieta</p>
           </div>
-          <div className="flex flex-col items-center justify-center gap-2 bg-red-light py-5 px-2 rounded-lg">
+          <div className="flex flex-col items-center justify-center gap-2 bg-red-light px-2 py-5 rounded-lg">
             <span className="text-gray-2 text-3xl font-bold">10</span>
-            <p className="text-gray-3 ">refeições fora da dieta</p>
+            <p className="text-gray-3 text-[14px]">refeições fora da dieta</p>
           </div>
         </div>
       </div>
