1955779455dd086312f909cc52f0638be17462eb
IgoorToorres <igorevangelista121@gmail.com>
2026-03-16 21:10:28 -0300

    config: biome instalado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
 export default function Home() {
   return (
     <>
-      <p className="text-gray-2 font-normal">
-        Texto regular
-      </p>
-      <h1 className="text-(--font-size-32) font-bold">
-        Título em negrito
-      </h1>
-
-
-
-
-
+      <p className="text-gray-2 font-normal">Texto regular</p>
+      <h1 className="text-(--font-size-32) font-bold">Título em negrito</h1>
     </>
   )
 }
