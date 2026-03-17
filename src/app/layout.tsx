1ae953b50bbe86d2bf5320b7593df5cc99182f20
IgoorToorres <igorevangelista121@gmail.com>
2026-03-17 11:50:21 -0300

    fix: ajustes estyrutura codigo layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,20 @@
-import { Nunito_Sans } from "next/font/google";
-import "./globals.css";
+import { Nunito_Sans } from "next/font/google"
+import "./globals.css"
 
 const nunito = Nunito_Sans({
   subsets: ["latin"],
   weight: ["400", "700"],
   display: "swap",
-});
+})
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="pt-BR" className={nunito.className}>
       <body>{children}</body>
     </html>
-  );
+  )
 }
